b3dc7dab628eb96b7de4fa1e4208f41429e29cb5
John Nash <redd@redd.ink>
2019-04-29 19:12:16 +1000

    move logging setup

--- reddstacksocial/config.py
+++ reddstacksocial/config.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 from .version import __version__
 
 VERSION = __version__
+
+DEBUG = True
+
+def get_working_dir():
+    from os.path import expanduser
+    home = expanduser('~')
+
+    working_dir = os.path.join(home, '.reddstacksocial')
+
+    if not os.path.exists(working_dir):
+        os.makedirs(working_dir)
+
+    return working_dir
+
+
+logPath = get_working_dir()
+log = logging.getLogger()
+if len(log.handlers) == 0:
+    log.setLevel(logging.DEBUG if DEBUG else logging.INFO)
+
+if not os.path.isdir(logPath):
+    os.makedirs(logPath)
+logFileHandler = RotatingFileHandler(logPath + "/reddstack-social.log", maxBytes=50000000, backupCount=99)
+log_format = ('[%(asctime)s] [%(levelname)s] [%(module)s:%(lineno)d] (' + str(os.getpid()) + ') %(message)s' if DEBUG else '%(message)s')
+logfile_formatter = logging.Formatter(log_format)
+logFileHandler.setFormatter(logfile_formatter)
+log.addHandler(logFileHandler)
